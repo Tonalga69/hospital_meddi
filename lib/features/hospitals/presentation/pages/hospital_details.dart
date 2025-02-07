367b81c23075a8a0d8ea374c7d33a81baf7e57bc
Samuel Ramos <114710082+Tonalga69@users.noreply.github.com>
2025-02-06 18:18:01 -0600

    reponsive for tablets

--- lib/features/hospitals/presentation/pages/hospital_details.dart
+++ lib/features/hospitals/presentation/pages/hospital_details.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hospitales_meddi/core/presentation/widgets/responsive_builder.dart';
 import 'package:hospitales_meddi/features/hospitals/presentation/pages/mobile.dart';
-import 'package:hospitales_meddi/features/hospitals/presentation/pages/tablet.dart';
 
 import '../../domain/entities/hospital.dart';
 
@@ -16,7 +15,7 @@ class HospitalDetailsPage extends StatelessWidget {
       mobileBuilder: (p0) => HospitalDetailsMobilePage(
         entity: entity,
       ),
-      tabletBuilder: (p0) => HospitalDetailsTabletPage(
+      tabletBuilder: (p0) => HospitalDetailsMobilePage(
         entity: entity,
       ),
     );
--- lib/features/hospitals/presentation/pages/mobile.dart
+++ lib/features/hospitals/presentation/pages/mobile.dart
@@ -34,45 +34,56 @@ class HospitalDetailsMobilePage extends StatelessWidget {
         child: const HugeIcon(
             icon: HugeIcons.strokeRoundedAdd01, color: ThemeColors.white),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(kPadding),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(child: Image.network(entity.photo, height: 100)),
-              const SizedBox(height: 10),
-              Center(child: Image.network(entity.logo, height: 50)),
-              const SizedBox(height: 20),
-              SelectableText('Address: ${entity.address}',
-                  style: const TextStyle(fontSize: 16)),
-              SelectableText('Google Maps: ${entity.urlGoogleMaps}',
-                  style: const TextStyle(
-                      fontSize: 16, color: ThemeColors.lightBlue)),
-              SelectableText('Telephone: ${entity.telephone}',
-                  style: const TextStyle(fontSize: 16)),
-              SelectableText('Schedule: ${entity.schedule}',
-                  style: const TextStyle(fontSize: 16)),
-              if (entity.stateCode != null)
-                SelectableText('State Code: ${entity.stateCode}',
-                    style: const TextStyle(fontSize: 16)),
-              SelectableText('Municipality: ${entity.municipality}',
-                  style: const TextStyle(fontSize: 16)),
-              SelectableText('Observations: ${entity.observations}',
-                  style: const TextStyle(fontSize: 16)),
-              SelectableText('Insurance: ${entity.insurance.join(', ')}',
-                  style: const TextStyle(fontSize: 16)),
-              SelectableText('Created At: ${entity.createdAt.toLocal()}',
-                  style: const TextStyle(fontSize: 16)),
-              SelectableText('Updated At: ${entity.updatedAt.toLocal()}',
-                  style: const TextStyle(fontSize: 16)),
-              SelectableText('Disponible: ${entity.enabled ? "Si" : "No"}',
-                  style: TextStyle(
-                      fontSize: 16,
-                      color: entity.enabled ? Colors.green : Colors.red)),
-            ],
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            alignment: Alignment.center,
+            constraints: const BoxConstraints(
+                maxWidth: 600
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(kPadding),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(child: Image.network(entity.photo, height: 100)),
+                    const SizedBox(height: 10),
+                    Center(child: Image.network(entity.logo, height: 50)),
+                    const SizedBox(height: 20),
+                    SelectableText('Address: ${entity.address}',
+                        style: const TextStyle(fontSize: 16)),
+                    SelectableText('Google Maps: ${entity.urlGoogleMaps}',
+                        style: const TextStyle(
+                            fontSize: 16, color: ThemeColors.lightBlue)),
+                    SelectableText('Telephone: ${entity.telephone}',
+                        style: const TextStyle(fontSize: 16)),
+                    SelectableText('Schedule: ${entity.schedule}',
+                        style: const TextStyle(fontSize: 16)),
+                    if (entity.stateCode != null)
+                      SelectableText('State Code: ${entity.stateCode}',
+                          style: const TextStyle(fontSize: 16)),
+                    SelectableText('Municipality: ${entity.municipality}',
+                        style: const TextStyle(fontSize: 16)),
+                    SelectableText('Observations: ${entity.observations}',
+                        style: const TextStyle(fontSize: 16)),
+                    SelectableText('Insurance: ${entity.insurance.join(', ')}',
+                        style: const TextStyle(fontSize: 16)),
+                    SelectableText('Created At: ${entity.createdAt.toLocal()}',
+                        style: const TextStyle(fontSize: 16)),
+                    SelectableText('Updated At: ${entity.updatedAt.toLocal()}',
+                        style: const TextStyle(fontSize: 16)),
+                    SelectableText('Disponible: ${entity.enabled ? "Si" : "No"}',
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: entity.enabled ? Colors.green : Colors.red)),
+                  ],
+                ),
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
